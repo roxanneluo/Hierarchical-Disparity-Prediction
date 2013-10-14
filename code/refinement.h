9bd558e09f21013588cbd18926aaa92e7c00d0b8
yukiaya <baixuejiao2010@gmail.com>
2013-10-14 14:59:04 +0800

    1. Added some explnations for refinement function. 2. Rewrote the code to make it easy to read.

--- code/refinement.h
+++ code/refinement.h
@@ -5,59 +5,71 @@
 #include "mylib.h"
 
 template <class type>
-void find_stable_pixels(Grid<type> &disparity_left, Grid<type> &disparity_right,
-						Grid<type>& occlusion_left, Grid<type>& occlusion_right) {
+void find_stable_pixels(Grid<type> &disparity_left,
+                        Grid<type> &disparity_right,
+	                Grid<type>& occlusion_left,
+                        Grid<type>& occlusion_right) {
+  occlusion_left.reset(disparity_left.height, disparity_left.width);
+  occlusion_right.reset(disparity_right.height, disparity_right.width);
+  occlusion_left.zero();
+  occlusion_right.zero();
 
-	occlusion_left.reset(disparity_left.height, disparity_left.width);
-    occlusion_right.reset(disparity_right.height, disparity_right.width);
-	occlusion_left.zero();
-	occlusion_right.zero();
-	for (int i = 0; i < disparity_left.height; ++i) {
-		for (int j = 0; j < disparity_left.width; ++j) {
-			type d = disparity_left[i][j];
-			if (j - d >= 0) {
-				if (d == 0 || abs(disparity_right[i][j - d] - d) >= 1) {
-					occlusion_left[i][j] = 255;
-			    }
-			} else {
-				occlusion_left[i][j] = 255;
-			}
-			d = disparity_right[i][j];
-			if (j + d < disparity_right.width) {
-				if (d == 0 || abs(disparity_left[i][j + d] - d) >= 1) {
-					occlusion_right[i][j] = 255;
-				}
-			} else {
-				occlusion_right[i][j] = 255;
-			}
-		}
-	}
+  // Left-right consisty check.
+  // occlusion_left[i][j] is 0 if (i, j) is a stable pixel.
+  // If disparity_left[i][j] != disparity_right[i][j - d], then (i,j) is an
+  // unstable pixel and occlusion_left[i][j] equals to 255.
+  // Do it for disparity_right as same strategy.
+  for (int i = 0; i < disparity_left.height; ++i) {
+    for (int j = 0; j < disparity_left.width; ++j) {
+      type d = disparity_left[i][j];
+      if (j - d >= 0) {
+        if (d == 0 || abs(disparity_right[i][j - d] - d) >= 1) {
+          occlusion_left[i][j] = 255;
+        }
+      } else {
+        occlusion_left[i][j] = 255;
+      }
+
+      d = disparity_right[i][j];
+      if (j + d < disparity_right.width) {
+        if (d == 0 || abs(disparity_left[i][j + d] - d) >= 1) {
+          occlusion_right[i][j] = 255;
+        }
+      } else {
+        occlusion_right[i][j] = 255;
+      }
+    }
+  }
 }
 
 template <class type>
-void update_matching_cost(Array3<double>& cost_left, Array3<double>& cost_right,
-						  Grid<type>& disparity_left, Grid<type>& disparity_right,
-						  Grid<type>& occlusion_left, Grid<type>& occlusion_right) {
-	int arr = cost_left.array;
-	int h = cost_left.height;
-	int w = cost_left.width;
-	cost_left.zero();
-	cost_right.zero();
-	for (int i = 0; i < h; ++i) {
-		for (int j = 0; j < w; ++j) {
-			if (occlusion_left[i][j] == 0) {
-				for (int d = 0; d < arr; ++d) {
-					cost_left[d][i][j] = abs (d - disparity_left[i][j]);
-				}
-			}
-			if (occlusion_right[i][j] == 0) {
-				for (int d = 0; d < arr; ++d) {
-					cost_right[d][i][j] = abs (d - disparity_right[i][j]);
-				}
-			}
-		}
-	}
+void update_matching_cost(Array3<double>& cost_left,
+                          Array3<double>& cost_right,
+                          Grid<type>& disparity_left,
+                          Grid<type>& disparity_right,
+                          Grid<type>& occlusion_left,
+                          Grid<type>& occlusion_right) {
+  // If (i,j) is an unstable pixel, cost is 0 for all disparities.
+  // Otherwise update it.
+  int arr = cost_left.array;
+  int h = cost_left.height;
+  int w = cost_left.width;
+  cost_left.zero();
+  cost_right.zero();
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      if (occlusion_left[i][j] == 0) {
+        for (int d = 0; d < arr; ++d) {
+          cost_left[d][i][j] = abs (d - disparity_left[i][j]);
+        }
+      }
+      if (occlusion_right[i][j] == 0) {
+        for (int d = 0; d < arr; ++d) {
+          cost_right[d][i][j] = abs (d - disparity_right[i][j]);
+        }
+      }
+    }
+  }
 }
-
 
 #endif
