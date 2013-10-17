f41411602a4a9150a953ba67a6534b865cd2d5c7
yukiaya <baixuejiao2010@gmail.com>
2013-10-17 20:28:16 +0800

    Output the errormap of each disparity result in checker1.cpp.

--- code/checker1.cpp
+++ code/checker1.cpp
@@ -8,8 +8,9 @@ The output is easier for other program to read.
 
 ******************************************/
 
-Grid<int> out, ref;
+Grid<int> out, ref, disparity_errormap;
 int pad1 = 10, pad2 = 10;
+char errormap[80];
 // the area that is not compared 
 // pad1 is on top and bottom,
 // pad2 is on left and right.
@@ -33,15 +34,27 @@ int main(int args, char ** argv) {
     if (h != ref.height || w != ref.width)
         { puts("Failed"); return 0;}
 
+    // Update the errormap.
+    disparity_errormap.reset(h, w);
+    disparity_errormap.zero();
     int total = 0, correct = 0;
     for (int i = pad1; i < h - pad1; ++i) 
     for (int j = pad2; j < w - pad2; ++j) {
         if (ref[i][j] == 0) continue;
         ++total;
         // the trancated value should be tolerance * scale
-        if (mylib::ABS(ref[i][j] - out[i][j]) < tolerance * scale) 
+        if (mylib::ABS(ref[i][j] - out[i][j]) < tolerance * scale) { 
             ++correct;
+            disparity_errormap[i][j] = 255;
+        } else if (ref[i][j] - out[i][j] > 0) {
+            disparity_errormap[i][j] = 0;
+        } else {
+            disparity_errormap[i][j] = 127;
+        }
     }
     printf("%d %d\n", correct , total);
+    strncpy(errormap, argv[1], strlen(argv[1]) - 4);
+    strcat(errormap, "error.pgm");
+    save_image(errormap, disparity_errormap);
     return 0;
 }
