39a70e484d9982f1ef7deda0abf7d41ca6bcb591
yukiaya <baixuejiao2010@gmail.com>
2013-10-17 20:43:55 +0800

    We'd better use .out but not .exe in super_run.py

--- super_run.py
+++ super_run.py
@@ -9,9 +9,9 @@ DATASETS = (
     'Middlebury_others',
 )
 ALGORITHMS = (
-#    'Original',
-#    'RandTree',
-    'silly',
+    'Original',
+    'RandTree',
+#    'silly',
 )
 
 data_path = ''
@@ -28,7 +28,7 @@ def run_test_with_path(path, left_result, right_result) :
     with open(path + 'spec.txt','r') as f :
         para = f.read().split()
     what = subprocess.check_output([
-        'bin/' + algoritm + '.exe', 
+        'bin/' + algoritm + '.out', 
         path + 'left.ppm', path + 'right.ppm', 
         para[0], para[1],
         left_result, right_result,
