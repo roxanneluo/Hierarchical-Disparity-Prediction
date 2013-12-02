f66bb33354badbb9ee0a0637191e27c328bb4fef
yukiaya <baixuejiao2010@gmail.com>
2013-12-02 18:37:11 +0800

    Change the output folder of gen_interval_test. Update the results of average search range in a html file.

--- code/gen_interval_test.cpp
+++ code/gen_interval_test.cpp
@@ -32,7 +32,7 @@ void read_initial_cnt_matlab (char* dataset, int H, int W) {
 }
 
 void read_initial_cnt_c (char* dataset, int H) {
-  char path[300] = "all_prob_gen/Prob_Gen";
+  char path[300] = "all_prob_gen/Prob_Gen_";
 	strcat(path, dataset);
 	strcat(path, "__cnt_c++.txt");	
 	FILE* file = fopen(path, "r");
@@ -44,7 +44,7 @@ void read_initial_cnt_c (char* dataset, int H) {
 	fclose(file);
 }
 void read_interval (char* dataset, int H) {
-  char path[300] = "all_interval/";
+  char path[300] = "all_gen_interval/";
 	strcat(path, dataset);
 	strcat(path, "_interval.txt");	
 	FILE* file = fopen(path, "r");
@@ -57,10 +57,9 @@ void read_interval (char* dataset, int H) {
 	fclose(file);
 }
 int main(int args, char* argv[]) {
-  // compare.out teddy output1 output2 60
+  // gen_interval_test.out teddy 60
 	strcpy(file_name[0], argv[1]);
-  strcpy(file_name[1], argv[2]); 
-	large = atoi(argv[3]);
+	large = atoi(argv[2]);
   small = large / 2;
 
 	large++;
@@ -91,20 +90,23 @@ int main(int args, char* argv[]) {
   fprintf(file, "ALL ERROR: %.4lf\n", err);
 	fclose(file);
 	*/
+
 	read_interval(file_name[0], small);
 
-	FILE* file = fopen(file_name[1], "w");
+  //	FILE* file = fopen(file_name[1], "w");
 	double search = 0;
 	initial_cnt_c.normalize();
 	for (int i = 0; i < interval.height; ++i) {
     search += (interval[i][1] - interval[i][0]) * initial_cnt_c[i];
 	}
-	fprintf(file,
-			    "Average Search Range ( %d ): %.4lf %.4lf\%\n",
-			    large - 1,
-					search,
-					search * 100 / (large - 1));
-	fclose(file);
+	// fprintf(file,
+	// 		    "Average Search Range ( %d ): %.4lf %.4lf\%\n",
+	// 		    large - 1,
+	//				search,
+	//				search * 100 / (large - 1));
+	//fclose(file);
+
+	printf("%d %.6lf\n", large, search);
 
 	return 0;
 }
--- run_all_gen_interval_test.py
+++ run_all_gen_interval_test.py
@@ -8,26 +8,58 @@ DATASETS = (
 )
 
 algorithm = 'bin/gen_interval_test.out'
-output = 'all_interval/'
+output = 'all_gen_interval/'
+
+table = {}
 
 print 'Compare all initial cnt generated from c++ with matlab.\n'
 
-def run_compare(path, pic) :
+def run_interval_test(path, pic) :
 	para = 0
+	ret = []
 	with open(path + 'spec.txt', 'r') as f :
 		para = f.read().split()
-	what = subprocess.check_output([algorithm,
-			pic,
-			output + pic + '__average_search_range.txt',
-			para[0]])
-
+	try :
+		what = subprocess.check_output([algorithm,
+				pic,
+				para[0]])
+		tmp =  map(float, what.split())
+#		interval = tmp[0];
+#   print interval
+		rati = float(tmp[1]) * 100 / tmp[0];
+#		print rati
+		tmp.append(' %.6lf' % rati)
+		ret.append(tmp)
+	except CalledProcessError :
+		print 'null'
+		ret.append([0, 0, 'null'])
+	return ret
 
 for dataset in DATASETS :
+	table[dataset] = {}
 	pic_names = subprocess.check_output(['ls', 'testdata/' + dataset + '/']).split()
 	for picture in pic_names :
 		print '~~~' + algorithm + '@' + dataset + '/' + picture + '~~~'
 		thepath = 'testdata/' + dataset + '/' + picture + '/'
-		run_compare(thepath, picture)
-
+		results = run_interval_test(thepath, picture)
+		table[dataset][picture] = results 
 print 'DONE.\n'
 
+report = open('bin/GenerateDisparityIntervalTest.html', 'w')
+report.write('<!DOCTYPE html>\n<html>\n<boby>\n')
+
+report.write('<h1>Average Search Range Based On Generated Disaprity Range</h1>\n')
+report.write('<table border = "3">\n')
+report.write('<tr><td>Test Data</td>')
+for side in ('Avg. Disparity Interval', 'Avg. Search Ratio') :
+	report.write('<td colspan = "1">' + side + '</td>')
+report.write('</tr>\n')
+
+for dataset in DATASETS :
+	pic_names = subprocess.check_output(['ls', 'testdata/' + dataset + '/']).split()
+	for picture in pic_names :
+		report.write('<tr><td>' + dataset + '.' + picture + ' (' + str(int(table[dataset][picture][0][0])) + ')' + '</td>')
+		report.write('<td>' + str(table[dataset][picture][0][1]) + '</td>')
+		report.write('<td>' + str(table[dataset][picture][0][2]) + ' % ' + '</td>')
+		report.write('</tr>\n')
+report.write('</html>')
