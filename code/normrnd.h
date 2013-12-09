77a9d97cf93b553507c2a06c230f618d67081dcc
xuan luo <roxanneluoxuan@gmail.com>
2013-12-09 21:35:34 +0800

    add function genGMM_layer in normrnd.h

--- code/normrnd.h
+++ code/normrnd.h
@@ -20,6 +20,29 @@ void genGMM(Array1<double> &gmm, int K, double const *mu, double const *sigma, d
 %     mu = [0.2071;-2.2668];
 %     Pi = [0.7719;0.2281];
 %     sigma^2 = [0.4155;92.1834];
+mu	-1.086022171	0.184231375
+sigma	5.680955165	0.543865616
+Pi	0.225260819	0.774739181
+**/
+void genGMM_layer(Array1<double> &gmm, int large_layer) {
+    if (large_layer == 0) {
+        double mu[2] = {0.2071,-2.2668};
+        double pi[2] = {0.7719,0.2281};
+        double sigma[2] = {0.6446, 9.6012};
+        int k = 2;
+        genGMM(gmm,k,mu,sigma,pi);
+    } else {
+        double mu[2] = {-1.086022171,0.184231375};
+        double pi[2] = {0.225260819,0.774739181};
+        double sigma[2] = {5.680955165, 0.543865616};
+        int k = 2;
+        genGMM(gmm,k,mu,sigma,pi);
+    }
+}
+/**
+%     mu = [0.2071;-2.2668];
+%     Pi = [0.7719;0.2281];
+%     sigma^2 = [0.4155;92.1834];
 **/
 void genGMM(Array1<double> &gmm) {
     double mu[2] = {0.2071,-2.2668};
@@ -41,7 +64,7 @@ void genGMM(Array1<double> &gmm) {
 %     sigma = [0.544941882;5.536860102];
 %     K=2;
 **/
-void genGMM(Array1<double> &gmm, int rem) {
+void genGMMRem(Array1<double> &gmm, int rem) {
     assert(0<=rem && rem < 2);
     double mu[2][2] = {{0.138324438,7.344032560}, {0.780102537,2.868775577}};
     double pi[2][2] = {{0.775804385,0.224195615}, {0.868242368,0.131757632}};
