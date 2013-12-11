b18a24444b14e38dbc251706fb69541bbba7c4d4
xuan luo <roxanneluoxuan@gmail.com>
2013-12-11 17:31:13 +0800

    change normrnd.h to gmm fitting data with fewer median filter

--- code/normrnd.h
+++ code/normrnd.h
@@ -20,16 +20,35 @@ void genGMM(Array1<double> &gmm, int K, double const *mu, double const *sigma, d
 %     mu = [0.2071;-2.2668];
 %     Pi = [0.7719;0.2281];
 %     sigma^2 = [0.4155;92.1834];
+
 mu	-1.086022171	0.184231375
+sigma	5.680955165	0.543865616
+Pi	0.225260819	0.774739181
+**/
+/**
+--------------data with new filter-----------
+
+--10--
+mu	0.990587103	0.187743358	-4.048706689
+sigma	2.912734757	0.606497154	11.087372705
+Pi	0.111202599	0.727640398	0.161157002
+
+--21--
+-1.086022171	0.184231375
 sigma	5.680955165	0.543865616
 Pi	0.225260819	0.774739181
 **/
 void genGMM_layer(Array1<double> &gmm, int large_layer) {
     if (large_layer == 0) {
-        double mu[2] = {0.2071,-2.2668};
+        /*double mu[2] = {0.2071,-2.2668};
         double pi[2] = {0.7719,0.2281};
         double sigma[2] = {0.6446, 9.6012};
         int k = 2;
+        genGMM(gmm,k,mu,sigma,pi);*/
+        const int k = 3;
+        double mu[k] = {0.990587103,0.187743358,-4.048706689};
+        double pi[k] = {0.111202599,0.727640398,0.161157002};
+        double sigma[k] = {2.912734757, 0.606497154,11.087372705};
         genGMM(gmm,k,mu,sigma,pi);
     } else {
         double mu[2] = {-1.086022171,0.184231375};
