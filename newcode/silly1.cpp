6974dede4f7562b90260387635a1c0747db1372d
Say.Uyas.Li <li3shuo1@gmail.com>
2014-05-08 00:07:06 +0800

    removed intermediant output

--- newcode/silly1.cpp
+++ newcode/silly1.cpp
@@ -42,15 +42,14 @@ int main(int args, char ** argv) {
         puts("Image too big. change settings.h and re-compile.");
         return 0;
     }
-/*
+
     for (int i = 0; i + 1 < levels; ++i) {
         left[i].shrinkPicture(left[i + 1]);
         right[i].shrinkPicture(right[i + 1]);
 
-        save_image_rgb(shrinkname[i + 1][0], left[i + 1].rgb, left[i+1].H, left[i+1].W);
-        save_image_rgb(shrinkname[i + 1][1], right[i + 1].rgb, left[i+1].H, left[i+1].W);
+        //save_image_rgb(shrinkname[i + 1][0], left[i + 1].rgb, left[i+1].H, left[i+1].W);
+        //save_image_rgb(shrinkname[i + 1][1], right[i + 1].rgb, left[i+1].H, left[i+1].W);
     }
-*/
 timer.reset();
     for (int lvl = levels - 1; lvl >= 0; -- lvl) {
 
@@ -63,27 +62,24 @@ timer.reset();
                                 left[lvl].H, left[lvl].W, max_disparity / (1 << lvl));
             dpf::getProbMatrix(lvl, max_disparity / (1 << (lvl + 1)), max_disparity / (1 << lvl));
             dpf::getInterval(0.001 * (1 << lvl));
-            // printf("# %d\n", dpf::height);
-            // for (int i = 0; i <= dpf::height; ++i) printf("%d %d\n", dpf::interval[i][0], dpf::interval[i][1]);
         
             left[lvl].readPrediction(left[lvl + 1].disparity);
             //right[lvl].readPrediction(right[lvl + 1].disparity);
         } 
         // Now use the INTERVAL to find the new disparities.
-// timer.check("prediction");
+
         left[lvl].buildForest(0.95);
         //right[lvl].buildForest(0.95);
-// timer.check("build forest");
         initDisparity(left[lvl], right[lvl]);
         updateTable(255 * 0.1);
         left[lvl].steroMatch(right[lvl], 1);
         //right[lvl].steroMatch(left[lvl], -1);
-// timer.check("steromatch");
 
         misc::median_filter(left[lvl].disparity, left[lvl].H, left[lvl].W);
- /*
-        misc::median_filter(right[lvl].disparity, right[lvl].H, right[lvl].W); 
+        //misc::median_filter(right[lvl].disparity, right[lvl].H, right[lvl].W); 
 
+        // the following is the refinement part
+ /*
         findStablePixels(left[lvl], right[lvl]);
         initDisparity(left[lvl], right[lvl]);
         updateTable(255 * 0.05);
@@ -92,7 +88,6 @@ timer.reset();
 
         misc::median_filter(left[lvl].disparity, left[lvl].H, left[lvl].W);
         misc::median_filter(right[lvl].disparity, right[lvl].H, right[lvl].W); 
-// timer.check("refine");
 
         save_image(layername[lvl][0], left[lvl].disparity, left[lvl].H, left[lvl].W, scale * (1 << lvl));
         save_image(layername[lvl][1], right[lvl].disparity, right[lvl].H, right[lvl].W, scale * (1 << lvl));
