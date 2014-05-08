5eca5678d2c79d70a250cbcf2d3f7c35264c5f3a
Say.Uyas.Li <li3shuo1@gmail.com>
2014-05-09 00:56:52 +0800

    Terrible code. Unfinished. Sleepy.

--- super_run_nonocc_old.py
+++ super_run_nonocc_old.py
@@ -11,63 +11,118 @@ DATASETS = (
 #    'Middlebury_bad',
 )
 ALGORITHMS = (
+#    'STCostAggre',
+#    'Original',
+#    'MSF_ALL_MST',
+#		'RandTreeEdgeAware',
+#    'ST',    
+#    'MSF_ALL_ST',
+#    'RandTree',
+#		'RandTree_ust',
+#    'MST_blind',
+#       'MSF2',
+#     'MSF2_TEST',
+#     'MSF2_LAST_RAND',
+#    'silly',
+#    'mst',
+#    'dpf-mst',
+#    'st',
+#    'dpf-st',
     'rt',
     'dpf-rt',	
 )
-CHECKER = 'checker.out'
 
 data_path = ''
-pic_names = '' 
+pic_names = '' # 
+left_result = ''
+right_result = ''
 total = 0
 correct = 0
 table = {}
 html_name = 'bin/SuperReport.html'
 
-def check_results(algo, path) : #, left_result = 'leftdisp.pgm', right_result = 'rightdisp.pgm') :
-    global correct
-    global total
+def run_ST_with_path(path, output) :
+    """ path should end with a / """
     para = 0
     with open(path + 'spec.txt','r') as f :
         para = f.read().split()
-    output = ('bin/left.pgm', 'bin/right.pgm')
-
-    #run the algorithm
-    subprocess.check_output([
-        'bin/' + algo + '.out', 
+    what = subprocess.check_output([
+        'bin/' + algoritm + '.out', 
         path + 'left.ppm', path + 'right.ppm', 
-        para[0], para[1],
-        output[0], output[1],
+        output,
+        para[0],para[1],
+        '0.1',
+        'ST-2',
     ])
 
-    tolerance = '1'
-    command = ['bin/'+CHECKER, 
-        path + 'displeft.pgm', path + 'dispright.pgm',
-        tolerance, para[1]]
+def run_test_with_path(path, left_result, right_result) :
+    """ path should end with a / """
+    para = 0
+    with open(path + 'spec.txt','r') as f :
+        para = f.read().split()
+    what = subprocess.check_output([
+        'bin/' + algoritm + '.out', 
+        path + 'left.ppm', path + 'right.ppm', 
+        para[0], para[1],
+        left_result, right_result,
+    ])
 
-    res = subprocess.check_output(command)
-    tmp = map(int, res.split())
-    correct = correct + tmp[0]
-    total = total + tmp[1]
-    rati = float(tmp[0]) / tmp[1]
-    print rati
-    return tmp
+def check_results(path) : #, left_result = 'leftdisp.pgm', right_result = 'rightdisp.pgm') :
+    global correct
+    global total
+    para = 0
+    tolerance = '1'
+    ret = []
+    with open(path + 'spec.txt','r') as f :
+        para = f.read().split()
+    output = ('bin/left.pgm', 'bin/right.pgm')
+    #for side, filename in zip(('left', 'right'), (left_result, right_result)) :
+    if algoritm == 'STCostAggre' :
+        run_ST_with_path(thepath, output[0])
+        run_ST_with_path(thepath, output[1])
+    else :
+        run_test_with_path(thepath, output[0], output[1])
+    for side, outfile in zip( ('left','right'), output ) :
+        left_or_right = '0'
+        if side == 'right' :
+          left_or_right = '1'
+        try :
+            res = subprocess.check_output(['bin/checker_nonocc.out',
+                                              outfile, 
+                                              path + 'displeft.pgm',
+																							path + 'dispright.pgm',
+                                              tolerance,
+                                              para[1],
+																							left_or_right])
+            tmp = map(int, res.split())
+            correct = correct + tmp[0]
+            total = total + tmp[1]
+            rati = float(tmp[0]) / tmp[1]
+            print rati
+            tmp.append(' %.6f ' % (1.0 - rati))
+            ret.append(tmp)
+        except CalledProcessError :
+#            print 'null'
+            ret.append([0,0, 'null'])
+    return ret
 
 for algoritm in ALGORITHMS :
     table[algoritm] = {}
     total = 0
     correct = 0
-    temp0 = (correct, total)
     for dataset in DATASETS :
         table[algoritm][dataset] = {}
         pic_names = subprocess.check_output(['ls', 'testdata/' + dataset + '/']).split()
-        temp = (correct, total)
+        temp = (total, correct)
         for picture in pic_names :
             print "~~~ "+ algoritm + '@' + dataset + '/' + picture +" ~~~"
+            #left_result= 'bin/' + algoritm + '_' + picture + '_left.pgm'
+            #right_result='bin/' + algoritm + '_' + picture + '_right.pgm'
             thepath = 'testdata/' + dataset + '/' + picture + '/'
-            results = check_results(algoritm, thepath)
+            results = check_results(thepath)
             table[algoritm][dataset][picture] = results
         table[algoritm][dataset]['Overall'] = [correct - temp[0], total - temp[1]]
-    table[algoritm]['Overall'] = [correct - temp0[0], total - temp0[1]]
+    table[algoritm]['Overall'] = [correct, total]
 
 print '\nDone.'
 
@@ -106,7 +161,7 @@ report.write('<h1>Dataset Detail</h1>\n')
 report.write('<table border="3">\n')
 report.write('<tr><td>Test Data</td>')
 for algoritm in ALGORITHMS :
-    report.write('<td>'+algoritm+'</td>')
+    report.write('<td colspan="2">'+algoritm+'</td>')
 report.write('</tr>\n')
 
 for dataset in DATASETS :
